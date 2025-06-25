d5cc4375775ec9088ea671080553c30d884a1720
codephusion <codephusiongene>
2025-06-25 18:43:47 +0200

    new wp site deployed

--- wp-content/plugins/site-mailer/assets/build/reviews.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => 'da98d5552167b6d71f82');
